049fe0a560b73be12bfbc3038979fd0eddd4d9e4
Eric Jackson <ejackson@ashevillenc.gov>
2022-02-20 16:08:44 -0500

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+ conda create -n jupyter python=3
+ conda activate jupyter
+ pip install notebook
+ conda install pandas
+ conda install sqlalchemy psycopg2
+ conda install -c conda-forge python-dotenv
+ conda install matplotlib
+ jupyter notebook&
